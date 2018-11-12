f5c048d427d6261242902027b73f755bee6aaa2f
svanterosenlind <svante.rosenlind@gmail.com>
2018-11-12 15:53:55 +0100

    More boats, and they work

--- battleship.py
+++ battleship.py
@@ -1,14 +1,13 @@
 import numpy as np
 from numpy.core.multiarray import ndarray
 import math
-FPS = 30
+import random
+FPS = 10
 
 class Battleship:
-    pos: ndarray
-    vel: ndarray
-    acc: ndarray
+    def __init__(self, sea):
+        self.sea = sea
 
-    def __init__(self):
         self.pos = np.array([400, 400])
         self.vel = np.array([0, 0])
         self.acc = np.array([0, 0])
@@ -29,42 +28,23 @@ class Battleship:
         self.angle_fric = 1000000
 
         self.shot_deviation = 0.1
+
     def update(self):
         self.vel = self.vel + self.acc/FPS
         self.pos = self.pos + self.vel/FPS
         self.angle_vel += self.angle_acc/FPS
         self.angle += self.angle_vel/FPS
 
-    def corners(self) -> ndarray:
-        rotator = np.array([[math.cos(self.angle), math.sin(self.angle)], [-math.sin(self.angle), math.cos(self.angle)]])
-        p1 = self.pos + np.dot(rotator, np.array([-self.size[0]/3, self.size[1]/2]))
-        p2 = self.pos + np.dot(rotator, np.array([2 * self.size[0] / 3, self.size[1] / 2]))
-        p3 = self.pos + np.dot(rotator, np.array([2 * self.size[0] / 3, - self.size[1] / 2]))
-        p4 = self.pos + np.dot(rotator, np.array([-self.size[0]/3, - self.size[1]/2]))
-        return np.array([p1, p2, p3, p4])
-
-    def shoot_left(self):
-        corn = self.corners()
-        p1 = (3 * corn[3] + corn[2])/4
-        p2 = (corn[3] + corn[2]) / 2
-        p3 = (3 * corn[2] + corn[3]) / 4
-        return [Cannonball(p1, self.angle + math.pi/2 + self.shot_deviation),
-                Cannonball(p2, self.angle + math.pi/2),
-                Cannonball(p3, self.angle + math.pi/2 - self.shot_deviation)]
-
-    def shoot_right(self):
-        corn = self.corners()
-        p1 = (3 * corn[0] + corn[1]) / 4
-        p2 = (corn[0] + corn[1]) / 2
-        p3 = (3 * corn[1] + corn[0]) / 4
-        return [Cannonball(p1, self.angle - math.pi/2 - self.shot_deviation),
-                Cannonball(p2, self.angle - math.pi/2),
-                Cannonball(p3, self.angle - math.pi/2 + self.shot_deviation)]
-
-
-class PlayerBattleship(Battleship):
-    def __init__(self):
-        super().__init__()
+        #   looparound in the x direction
+        if self.pos[0] < 0:
+            self.pos[0] = self.sea[0]
+        elif self.pos[0] > self.sea[0]:
+            self.pos[0] = 0
+        #   looparound in the y direction
+        if self.pos[1] < 0:
+            self.pos[1] = self.sea[1]
+        elif self.pos[1] > self.sea[1]:
+            self.pos[1] = 0
 
     def calculate_FM(self, keys):
         [up, right, left] = keys
@@ -101,13 +81,102 @@ class PlayerBattleship(Battleship):
             return self.shoot_right()
         return []
 
+
+    def corners(self) -> ndarray:
+        rotator = np.array([[math.cos(self.angle), math.sin(self.angle)], [-math.sin(self.angle), math.cos(self.angle)]])
+        p1 = self.pos + np.dot(rotator, np.array([-self.size[0]/3, self.size[1]/2]))
+        p2 = self.pos + np.dot(rotator, np.array([2 * self.size[0] / 3, self.size[1] / 2]))
+        p3 = self.pos + np.dot(rotator, np.array([2 * self.size[0] / 3, - self.size[1] / 2]))
+        p4 = self.pos + np.dot(rotator, np.array([-self.size[0]/3, - self.size[1]/2]))
+        return np.array([p1, p2, p3, p4])
+
+    def shoot_left(self):
+        corn = self.corners()
+        p1 = (3 * corn[3] + corn[2])/4
+        p2 = (corn[3] + corn[2]) / 2
+        p3 = (3 * corn[2] + corn[3]) / 4
+        return [Cannonball(p1, self.angle + math.pi/2 + self.shot_deviation, self),
+                Cannonball(p2, self.angle + math.pi/2, self),
+                Cannonball(p3, self.angle + math.pi/2 - self.shot_deviation, self)]
+
+    def shoot_right(self):
+        corn = self.corners()
+        p1 = (3 * corn[0] + corn[1]) / 4
+        p2 = (corn[0] + corn[1]) / 2
+        p3 = (3 * corn[1] + corn[0]) / 4
+        return [Cannonball(p1, self.angle - math.pi/2 - self.shot_deviation, self),
+                Cannonball(p2, self.angle - math.pi/2, self),
+                Cannonball(p3, self.angle - math.pi/2 + self.shot_deviation, self)]
+
+
+class DNABattleship(Battleship):
+    def __init__(self, sea):
+        super().__init__(sea)
+        self.DNA = [[0]*4]*4
+        for x in range(4):
+            for y in range(4):
+                gene = np.array([random.uniform(-1, 1)] * 2 + [random.random()] * 2)    # acc, angle, shootleft, right
+                self.DNA[x][y] = gene
+        self.pos = np.array([random.randint(0, sea[0]), random.randint(0, sea[1])])
+
+    def calculate(self, boats):
+        total_gene = np.array([0]*4)
+        for boat in boats:
+            if boat is self:
+                continue
+            if np.dot(boat.pos - self.pos, boat.pos - self.pos) > 10000:
+                continue
+            rel_pos = boat.pos - self.pos
+            rotator = np.array(
+                [[math.cos(self.angle), math.sin(self.angle)], [-math.sin(self.angle), math.cos(self.angle)]])
+            rel_pos_rotated = np.dot(rel_pos, rotator)
+            gene_index = ((rel_pos_rotated - np.array([100]*2)) // 50).tolist()
+            gene = self.DNA[int(gene_index[0])][int(gene_index[1])]   # BUG in pycharm
+            total_gene = total_gene + gene
+        [up, left, right, a, d] = [False] * 5
+        if 0.5 < total_gene[2] > total_gene[3]:
+            a = True
+        elif 0.5 < total_gene[3] > total_gene[2]:
+            d = True
+        if total_gene[0] > 0.7:
+            up = True
+        if total_gene[1] > 0.3:
+            left = True
+        elif total_gene[1] < -0.3:
+            right = True
+        super().calculate_FM([up, right, left])
+        return super().shoot([a, d])
+
+
+class PlayerBattleship(Battleship):
+    def __init__(self, sea):
+        super().__init__(sea)
+
+    def calculate(self, keys):
+        [up, right, left, a, d] = keys
+        super().calculate_FM([up, right, left])
+        return super().shoot([a, d])
+
 class Cannonball:
-    def __init__(self, pos, angle):
+    def __init__(self, pos, angle, boat):
         self.pos = pos
         self.angle = angle
         self.vel = 5
         self.distance_left = 60
+        self.boat = boat
 
     def update(self):
         self.pos = self.pos + np.array([self.vel]*2) * np.array([math.cos(self.angle), -math.sin(self.angle)])
         self.distance_left -= self.vel
+
+    def check_collision(self, b):
+        if b is not self.boat:
+            rel_pos = b.pos - self.pos
+            rotator = np.array(
+                [[math.cos(b.angle), math.sin(b.angle)], [-math.sin(b.angle), math.cos(b.angle)]])
+            rel_pos_rotated = np.dot(rel_pos, rotator)
+            if -b.size[0]/3 < rel_pos_rotated[0] < 2*b.size[0]/3 and -b.size[1]/2 < rel_pos_rotated[1] < b.size[1]/2:
+                return True
+        else:
+            return False
+
--- battleship_client.py
+++ battleship_client.py
@@ -5,12 +5,17 @@ from battleship import *
 def main():
     #   Initialize graphics and things
     pygame.init()
-    scr = pygame.display.set_mode((800, 800))
+    sea = (800, 800)
+    scr = pygame.display.set_mode(sea)
     running = True
     [up, right, left, a, d] = [False]*5
     cl = pygame.time.Clock()
     #   Initialize things
-    b = PlayerBattleship()
+    b = PlayerBattleship(sea)
+    boats = []
+    for n in range(20):
+        boot = DNABattleship(sea)
+        boats.append(DNABattleship(sea))
     balls = []
 
     while running:
@@ -42,31 +47,50 @@ def main():
                 elif event.key == pygame.K_d:
                     d = False
         # Process the events
-        balls += b.shoot([a, d])
+
         for ball in balls[::-1]:
             ball.update()
+            remove = False
+            for boat in boats[::-1]:
+                if ball.check_collision(boat):
+                    remove = True
+                    boats.remove(boat)
+                    break
             if ball.distance_left < 0:
+                remove = True
+            if remove:
                 balls.remove(ball)
-        b.calculate_FM([up, right, left])
+        balls += b.calculate([up, right, left, a, d])
         b.update()
+
+        for boat in boats:
+            balls += boat.calculate(boats + [b])
+            boat.update()
 
         # Draw the sea
         pygame.draw.rect(scr, (0, 0, 0), scr.get_rect())
         draw_boat(b, scr)
+        for boat in boats:
+            draw_boat(boat, scr)
         for ball in balls:
             draw_ball(ball, scr)
         pygame.display.flip()
+
 
 def draw_boat(b, screen):
     corn = b.corners()
     p1 = tuple(corn[0].tolist())
     p2 = tuple(((corn[1] + corn[2])/2).tolist())
     p3 = tuple(corn[3].tolist())
-    pygame.draw.polygon(screen, (200, 50, 40), [p1, p2, p3])
+    if type(b) == PlayerBattleship:
+        color = (200, 50, 40)
+    else:
+        color = (120, 50, 150)
+    pygame.draw.polygon(screen, color, [p1, p2, p3])
 
 
 def draw_ball(b, screen):
-    pygame.draw.circle(screen, (100, 0, 0), tuple(b.pos.astype(int).tolist()), 5)
+    pygame.draw.circle(screen, (100, 0, 0), tuple(b.pos.astype(int).tolist()), 3)
 
 if __name__ == '__main__':
     main()
